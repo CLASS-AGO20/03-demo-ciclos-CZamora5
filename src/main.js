92aee29ebf3a2f1569165658bee8ed9749a7096e
Carlos Zamora <czamora5@ucol.mx>
2020-12-07 02:57:41 -0600

    Reimplemented sumarParesFor

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ export default class App {
         let total = 0;
         for(let i=1; i<=20; i++){
             if(i%2==0){
-                total++;
+                total += i;
             }
         }
         return total;
@@ -14,4 +14,4 @@ let app = new App();
 
 //Prueba del método sumarParesFor
 console.log(`Probando método sumarParesFor()`);
-console.log(`Entre el número 1 y el número 20 hay ${app.sumarParesFor()} números pares`);
+console.log(`La suma de los números pares entre el número 1 y el número 20 es ${app.sumarParesFor()}`);
