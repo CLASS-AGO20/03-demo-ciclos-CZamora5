e9c3855fb297d96f343ba6dc0f70ec4a36e759de
Carlos Zamora <czamora5@ucol.mx>
2020-12-07 02:51:03 -0600

    Implemented sumarParesFor method

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default class App {}
+export default class App {
+    sumarParesFor(){
+        let total = 0;
+        for(let i=1; i<=20; i++){
+            if(i%2==0){
+                total++;
+            }
+        }
+        return total;
+    }
+}
 
 let app = new App();
