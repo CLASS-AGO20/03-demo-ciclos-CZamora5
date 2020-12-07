fa3240bac635147e2f0bce95998511fe49a1c23d
Carlos Zamora <czamora5@ucol.mx>
2020-12-07 03:04:48 -0600

    Implemented contarImparesFor method

--- src/main.js
+++ src/main.js
@@ -8,6 +8,22 @@ export default class App {
         }
         return total;
     }
+
+    contarImparesFor(inicio, fin){
+
+        if(inicio>fin){
+            inicio += fin;
+            fin = inicio - fin;
+            inicio = inicio -fin;
+        }
+        let total = 0;
+        for(let i=inicio; i<=fin; i++){
+            if(i%2==1){
+                total++;
+            }
+        }
+        return total;
+    }
 }
 
 let app = new App();
