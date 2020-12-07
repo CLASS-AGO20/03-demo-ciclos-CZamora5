e665e5c37f08c3ebadd95569da2faf350aeeefbf
Carlos Zamora <czamora5@ucol.mx>
2020-12-07 03:10:06 -0600

    Included test cases for contarImparesFor method

--- src/main.js
+++ src/main.js
@@ -31,3 +31,11 @@ let app = new App();
 //Prueba del método sumarParesFor
 console.log(`Probando método sumarParesFor()`);
 console.log(`La suma de los números pares entre el número 1 y el número 20 es ${app.sumarParesFor()}`);
+
+//Prueba del método sumarParesFor
+let a = 23;
+let b = 345;
+console.log(`Probando método contarImparesFor(inicio, fin)`);
+console.log(`En el intervalo [${a},${b}] hay ${app.contarImparesFor(a, b)} números impares`);
+console.log(`En el intervalo [${0},${3*a}] hay ${app.contarImparesFor(0, 3*a)} números impares`);
+console.log(`En el intervalo [${2*a},${2*b}] hay ${app.contarImparesFor(2*b, 2*a)} números impares`);
